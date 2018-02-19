84be54202d965bdf3f9b6bffcd432f4ed6c317bc
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-02-19 15:43:54 -0500

    Create individual components

--- js/index.js
+++ js/index.js
@@ -6,12 +6,9 @@ const results = document.querySelector('.results');
 // document.getElementById('loader').style.display = 'none';
 
 function fetchData(e) {
-  // console.log('fetching data...'); // for dev only, DELETE
-
   if (input.value !== '') {
     // rearrange elements (when request sent) maybe add spinner/loader
 
-    // ajax request
     const wikiUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${input.value}&limit=20&format=json&origin=*`;
 
     fetch(wikiUrl).then((response) => {
@@ -28,18 +25,6 @@ function fetchData(e) {
 }
 
 function displayResults(data) {
-  // append links to dom (when promise fullfilled
-  /* const html = data[1].map((element, i) => {
-    return (`
-      <li class="results__item">
-        <a class="results__link" href=${data[3][i]}>
-          <h2 class="results__title">${element}</h2>
-          <p class="results__text">${data[2][i]}</p>
-        </a>
-      </li>
-    `);
-  }).join(''); */
-
   let html = '';
 
   data[1].forEach((element, i) => {
@@ -52,6 +37,19 @@ function displayResults(data) {
       </li>
     `;
   });
+
+  /* // Previous method
+  const html = data[1].map((element, i) => {
+    return (`
+      <li class="results__item">
+        <a class="results__link" href=${data[3][i]}>
+          <h2 class="results__title">${element}</h2>
+          <p class="results__text">${data[2][i]}</p>
+        </a>
+      </li>
+    `);
+  }).join(''); */
+
   // append lis
   results.innerHTML = html;
   // slide in links section
---
+++ js/wiki.js
@@ -0,0 +1,11 @@
+class Wiki {
+  constructor() {
+    this.article_count = 20;
+  }
+
+  async getArticles(searchInput) {
+    const res = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchInput}&limit=${this.article_count}&format=json&origin=*`);
+    const resData = await res.json();
+    return resData;
+  }
+}
